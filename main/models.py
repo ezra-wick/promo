9e249d99ccde207bf3dfa2999806987f074e6a90
levgera <lev.gera@gmail.com>
2023-03-21 00:25:53 +0300

    Описание ветки в README

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.dateformat import *
+from django.utils import timezone
+import re
 
 class PageBlock(models.Model):
     title = models.CharField(max_length=255)
@@ -9,6 +12,33 @@ class PageBlock(models.Model):
     seo_keywords = models.CharField(max_length=255, null=True, blank=True)
     template = models.CharField(max_length=255)
 
+    class Meta:
+        verbose_name = 'Блок страницы'
+        verbose_name_plural = 'Блоки страницы'
+
     def __str__(self):
         return self.title
     
+class FeedBack(models.Model):
+    phone = models.CharField(max_length=16, verbose_name="Телефон", null=True, blank=True, unique=True)
+    name = models.CharField(max_length=50, verbose_name="Имя", null=True, blank=True, unique=True)
+    email = models.CharField(max_length=80, verbose_name="email", null=True, blank=True, unique=True)
+    text = models.CharField(max_length=500, verbose_name="Сообщение", null=True, blank=True)
+    datetime_create = models.DateTimeField(default=timezone.now, verbose_name="Дата и время создания")
+    complete = models.BooleanField(default=False, verbose_name="Отметка о выполнении")
+
+
+    def save_phone(self, phone):
+        phone = re.sub(r'\+?[78](\d{3})(\d{3})(\d\d)(\d\d)', r'+7\1\2\3\4', phone)
+        if phone[0] == '9' and len(phone) == 10:
+            phone = '+7' + phone
+
+        self.phone = re.sub(r'\+?[78](\d{3})(\d{3})(\d\d)(\d\d)', r'+7\1\2\3\4', phone)
+        self.username = re.sub(r'\+?[78](\d{3})(\d{3})(\d\d)(\d\d)', r'+7\1\2\3\4', phone)
+
+    class Meta:
+        verbose_name = 'Заявка'
+        verbose_name_plural = 'Заявки'
+
+    def __str__(self):
+        return self.name + " Дата и время создания: %s" % (format(self.datetime_create, "d.m.Y H:i"))
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from . import views
+from .views import *
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    # path('', views.index, name='index'),
+    # path('about/', views.about, name='about'),
+    path('', IndexPage.as_view(), name='index'),
+    path('about/', AboutUsView.as_view(), name='about'),
 ]
